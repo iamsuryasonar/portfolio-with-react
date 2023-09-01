8a01ecfb5b623176218afdf77e104778f581a0e3
Surya Sonar <iamsuryasonar@gmail.com>
2023-09-01 13:37:19 +0530

    ui fixes

--- src/App/Data/text.js
+++ src/App/Data/text.js
@@ -2,8 +2,8 @@ const text = {
     name: "Surya Sonar",
     greeting: 'Hello World!',
     introduction: "I'm Surya Sonar",
-    about_me: "I'm a dedicated software developer with a passion for crafting elegant and efficient solutions.My journey in the world of code began with a curiosity to build and innovate.As a developer, I thrive on challenges and believe in continuous learning.I am currently focused on honing my data structure and algorithm skills, aiming to create even smarter and more optimized software.",
-    skills_intro: "These are some languages,frameworks and tools that I have experience with.",
+    about_me: "I'm a dedicated software developer with a passion for crafting elegant and efficient solutions. My journey in the world of code began with a curiosity to build and innovate. As a developer, I thrive on challenges and believe in continuous learning. I am currently focused on honing my data structure and algorithm skills, aiming to create even smarter and more optimized software.",
+    skills_intro: "These are some languages, frameworks and tools that I have experience with.",
 }
 
 export default text;
